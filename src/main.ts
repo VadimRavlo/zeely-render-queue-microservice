71a939a85e1c05026126018bbee5fe10e390906c
Vadym Ravlo <wadyan.r@gmail.com>
2025-04-09 23:07:57 +0200

    Add Swagger (documentation) & helmet (security)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
+import { name, version } from '../package.json';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import helmet from "helmet";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  setupSwagger(app);
+  app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
+}
+
+function setupSwagger(app: NestExpressApplication): void {
+  const swaggerOptions = new DocumentBuilder()
+      .setTitle(`${name} API`)
+      .setDescription(`Documentation for ${name} API`)
+      .setVersion(version)
+      .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  SwaggerModule.setup('/documentation', app, document);
 }
 bootstrap();
